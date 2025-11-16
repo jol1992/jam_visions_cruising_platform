2539a57e140618362b94112a6572dd7e6c3f6445
jol1992 <mr.chiakajaylou@gmail.com>
2025-11-16 11:45:35 -0600

    feat: add mulitple themes

---
+++ src/styles/schemes.ts
@@ -0,0 +1,24 @@
+// color pallet: https://colorhunt.co/palette/154d711c6ea433a1e0fff9af
+
+export const default_theme = {
+  primary: "#FFFFFF",
+  secondary: "#33A1E0",
+  trimary: "#365a83",
+  accent: "#ffaf0a",
+};
+
+export const demoColors = {
+  accent: "#00FFDE",
+  accent2: "#4300FF",
+  wave: "#00CAFF",
+  gray: "#D9D9D9",
+  background: "#FFFFFF",
+};
+
+export const colormind_theme = {
+  white: "#FFFFFF",
+  black: "#22282C",
+  orange: "#FFAF0A",
+  dark_blue: "#365A83",
+  light_blue: "#33A1E0",
+};
