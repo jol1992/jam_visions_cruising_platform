8139db11ee746ab4b403a5987473eca44416f009
jol1992 <mr.chiakajaylou@gmail.com>
2025-11-17 06:56:08 -0600

    feat: add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import { CallToActionButton } from "./Components/Buttons/CallToActionButton";
 import { WelcomeVideo } from "./Components/Videos/WelcomeVideo";
+import jm_visions from "./assets/jam_visions.png";
 
 export const App = () => {
   return (
     <>
-      <CallToActionButton label="Im coming😊" />
+      <CallToActionButton label="Sign me up😊" />
+      <div className="jmLogo">
+        <img src={jm_visions} />
+      </div>
+
       <h1>Back on the Valor! 🎉</h1>
       <p>
         WE ARE SAILING ON THE CARNIVAL VALOR AGAIN IN 2026 AND WE HOPE THAT YOU
